dfb44116ce3b525315b1bfe60e2e5f1f15bcdb12
jaraction <ing.ajaramilloc@gmail.com>
2021-05-04 20:37:37 -0500

    HEADER´S LAYOUT

---
+++ views/modules/header.php
@@ -0,0 +1,181 @@
+<!-- TOP -->
+
+<div class="container-fluid upperBar" id="top">
+
+    <div class="container">
+    
+        <div class="row">
+        
+            <!-- SOCIAL MEDIA -->
+
+            <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+
+                <ul>
+                    <li>
+                        <a href="https://facebook.com/" target="blank">
+                            <i class="fa fa-facebook socialNetwork facebookWhite" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://youtube.com/" target="blank">
+                            <i class="fa fa-youtube socialNetwork youtubeWhite" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/" target="blank">
+                            <i class="fa fa-twitter socialNetwork twitterWhite" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://google.com/" target="blank">
+                            <i class="fa fa-google socialNetwork googleWhite" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://instagram.com/" target="blank">
+                            <i class="fa fa-instagram socialNetwork instagramWhite" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            
+            </div>
+
+            <!-- REGISTER -->
+
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 register">
+
+                <ul>
+                    <li><a href="#loginModal" data-toggle="modal">Log In</a></li>
+                    <li>|</li>
+                    <li><a href="#signupModal" data-toggle="modal">Sign In</a></li>
+                </ul>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!-- HEADER -->
+
+<header class="container-fluid">
+
+    <div class="container">
+
+        <div class="row" id="header">
+        
+            <!-- LOGO -->
+
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logo">
+                <a href="index.php">
+                    <img src="http://localhost/backend/views/img/template/logo.png" class="img-responsive" alt="logo">
+                </a>
+
+            </div>
+
+            <!-- CATEGORIES AND SEARCHER -->
+
+            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+
+                <!-- CATEGORIES BUTTON -->
+
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="categoriesBtn">
+                    <p>CATEGORIES
+                        <span class="pull-right">
+                            <i class="fa fa-bars" aria-hidden="true"></i>
+                        </span>
+                    </p>
+                </div>
+
+                <!-- SEARCHER -->
+
+                <div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="searcher">
+                    <input type="search" name="search" class="form-control" placeholder="Search...">
+                    <span class="input-group-btn">
+                        <a href="#">
+                            <button class="btn btn-default backColor" type="submit">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </a>
+                    </span>
+                </div>
+
+            </div>
+
+            <!-- SHOPPING CART -->
+
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="shoppingCart">
+                <a href="#">
+                    <button class="btn btn-default pull-left backColor">
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                    </button>
+                </a>
+                <p>YOUR CART <span class="cartAmount"></span> <br> USD $ <span class="cartTotal"></span></p>
+            </div>
+
+        </div>
+
+        <!-- CATEGORIES -->
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 backColor" id="categories">
+
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategories">Lorem Ipsum.</a>
+                </h4>
+                    <hr>
+                <ul>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategories">Lorem Ipsum.</a>
+                </h4>
+                    <hr>
+                <ul>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategories">Lorem Ipsum.</a>
+                </h4>
+                    <hr>
+                <ul>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategories">Lorem Ipsum.</a>
+                </h4>
+                    <hr>
+                <ul>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategories">Lorem Ipsum</a></li>
+                </ul>
+            </div>
+            
+        </div>
+        
+    </div>
+
+</header>
---
+++ views/template.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="Ecommerce">
+    <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit.">
+    <meta name="keyword" content="ecommerce, web, shop, online">
+
+    <title>Ecommerce</title>
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="views/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="views/css/plugins/font-awesome.min.css">
+</head>
+<body>
+
+    <?php
+
+    /* Header */
+
+    include "modules/header.php";
+
+    ?>
+
+    <!-- jQuery -->
+    <script src="views/js/plugins/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="views/js/plugins/bootstrap.min.js"></script>
+</body>
+</html>
